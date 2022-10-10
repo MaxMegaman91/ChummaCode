56bbec24dc86debc640e7bfe5cfa55aee8edc9c2
MaxMegaman91 <aarush.ar@gmail.com>
2022-10-10 15:43:01 -0400

    organizing(move to mac)

--- kinda cool projects/YT/main.py
+++ kinda cool projects/YT/main.py
@@ -1,17 +1,18 @@
 # Pls reprogram this
-# The module that does the work
+# Debug mode... requires more integration towards code
 DEBUG = False
+# Importing all Modules
 from pytube import YouTube
 from moviepy.editor import *
 import time 
 import os
 import sys
-import getpass
 
+availableThreads = 16 if os.name == "posix" else 32
 theofficialqual = 0
 
-def info_of_mp3(dictionary_of_info,filenamefull):
-    from mutagen.id3 import ID3, TIT2, TPE2, TALB, TPE1, TYER, TDAT, TRCK, TCON, TORY, TPUB, USLT
+def info_of_mp3(dictionary_of_info, filenamefull):
+    from mutagen.id3 import ID3, TIT2, TALB, TPE1
     audio = ID3(SAVE_PATH+"/"+filenamefull)
     
     audio.add(TIT2(encoding=3, text = dictionary_of_info["TIT2"]))    #TITLE
@@ -20,12 +21,12 @@ def info_of_mp3(dictionary_of_info,filenamefull):
 
     audio.save(v2_version=3)
 
-def mp4_to_mp3(mp4, mp3, remove_mp4):
+def mp4_to_mp3(mp4, mp3):
     mp4_without_frames = AudioFileClip(SAVE_PATH+"/"+mp4)
     mp4_without_frames.set_duration(yt.length)
     mp4_without_frames.write_audiofile(SAVE_PATH+"/"+mp3)
     mp4_without_frames.close()
-    if remove_mp4: os.remove(SAVE_PATH+"/"+mp4)
+    os.remove(SAVE_PATH+"/"+mp4)
 
 def download_by_itag(itag, finalname):
     global yt, SAVE_PATH, name
@@ -37,17 +38,16 @@ def download_by_itag(itag, finalname):
         sys.exit("Download error")
         pass
 
-def combine_audio_video(audiofile,videofile,output_file_name_with_extension):
-    download_path = SAVE_PATH
-    videoclip = VideoFileClip(download_path+"/"+videofile)
-    audioclip = AudioFileClip(download_path+"/"+audiofile)
+def combine_audio_video(audiofile, videofile, output_file_name_with_extension):
+    videoclip = VideoFileClip(SAVE_PATH+"/"+videofile)
+    audioclip = AudioFileClip(SAVE_PATH+"/"+audiofile)
     video = videoclip.set_audio(audioclip)
     if theofficialqual == "4k" or theofficialqual == "8k":
-        video.write_videofile(str(download_path+"/"+output_file_name_with_extension), fps=60, threads=16 if os.name == "posix" else 32, codec = "libfdk_aac")
-    else: video.write_videofile(str(download_path+"/"+output_file_name_with_extension), fps=30, threads=16 if os.name == "posix" else 32, codec = "libfdk_aac")
+        video.write_videofile(str(SAVE_PATH+"/"+output_file_name_with_extension), fps=60, threads=16 if os.name == "posix" else 32, codec = "libfdk_aac")
+    else: video.write_videofile(str(SAVE_PATH+"/"+output_file_name_with_extension), fps=30, threads=16 if os.name == "posix" else 32, codec = "libfdk_aac")
     while True: ### check if audio and video files are closed to delete them
         try:
-            myfile = open(f'{download_path}/{videofile}', "r+")
+            myfile = open(f'{SAVE_PATH}/{videofile}', "r+")
             myfile.close()
             os.remove("C:/Users/aarus/Downloaded_Youtube/tempaud.mp4")
             os.remove("C:/Users/aarus/Downloaded_Youtube/tempvid.mp4")
@@ -108,7 +108,7 @@ def download_with_qual(qual, finalname):
             auditag = yt.streams.filter(mime_type="audio/mp4").get_audio_only().itag
             print("Downloading itag: " + str(auditag))
             download_by_itag(auditag, "tempaud.mp4")
-            mp4_to_mp3("tempaud.mp4", finalname+".mp3", True)    
+            mp4_to_mp3("tempaud.mp4", finalname+".mp3")    
         print("Product installed and file ready!\n")
     except AttributeError: print("Attribute Error raised: I think you chose a quality not available!")
             
@@ -187,55 +187,33 @@ elif os.name != "nt":
 
 # Full loop
 while True:
+    name = ""
+
+
     os.system('cls' if os.name == 'nt' else 'clear')
+
     print("==========================================================================================")
-    try:link = input("Give the link of the video needed to be installed: -->  ") # link of the video
-    except EOFError: sys.exit("Goodbye! ")
-    if link != "txt" and link != "^Z" and len(link.split("|")) < 2:
-        name = input("What shall I name the file as? -->  ") #  File name
-    else: name = ""
-    product = input("Where is this going? --> ")
-    if product == "ios": m4v = True
-    else: m4v = False
+    textinput = input("Give the link of the video needed to be installed: -->  ") # link of the video
     
     # If we are reading the txt file
-    if link == "txt":
-        print("==========================================================================================\nReading file!\n\n")
+    if textinput == "the.txt":
+        print("==========================================================================================")
+        print("Reading file!" + "\n\n")
+
         # Read the text file
         with open("/home/aarush/YT/the.txt", "r") as f:
             mylist = [line.rstrip('\n') for line in f]
+            mylist = mylist[1:]
+        
         # and read line by line
-        lengthoftxt = 0
         for line in mylist:
-            findlink(line.split("|")[0]) # Split each line by the | sign, then find the link associated
-            download_with_qual(line.split("|")[1], name) # download with the quality on the other side of the |
-            needLyrics = False
-            if line.split("|")[1] == "mp3": # if its mp3, set name|artist|album
-                dictionary_of_info["TIT2"], dictionary_of_info["TPE1"], dictionary_of_info["TALB"]= line.split("|")[2:5]
-                if len(line.split("|")) == 6:
-                    needLyrics = bool(line.split("|")[6])
-                info_of_mp3(dictionary_of_info, name+".mp3")
-            if needLyrics:
-                from lyrics import getLyrics
-                songname, songartist = line.split("|")[2:4]
-                getLyrics(songname, songartist)
-    elif link == "^Z":sys.exit("Goodbye! ")
-    elif len(link.split("|")) >= 2:
-        findlink(link.split("|")[0]) # Split each line by the | sign, then find the link associated
-        download_with_qual(link.split("|")[1], name) # download with the quality on the other side of the |
-        needLyrics = False
-        if link.split("|")[1] == "mp3": # if its mp3, set name|artist|album
-            dictionary_of_info["TIT2"], dictionary_of_info["TPE1"], dictionary_of_info["TALB"]= link.split("|")[2:5]
-            if len(link.split("|")) == 6:
-                needLyrics = bool(link.split("|")[6])
-            info_of_mp3(dictionary_of_info, name+".mp3")
-        if needLyrics:
-            from lyrics import getLyrics
-            songname, songartist = link.split("|")[2:4]
-            getLyrics(songname, songartist)
+            link, quality = line.split("|")
+            findlink(link) # Split each line by the | sign, then find the link associated
+            download_with_qual(quality, name) # download with the quality on the other side of the |
+    
     # else if we are downloading only one
-    elif link[0:3] == "www" or link[0:3] == "htt":
-        findlink(link) # Find the link associated yt vid
+    elif textinput[0:3] == "www" or textinput[0:3] == "htt":
+        findlink(textinput) # Find the link associated yt vid
         itagtouse = input("What quality or itag would you like? -->  ") # Gathering the itag we require
         if itagtouse == "": # Auto download highest progressive source
             itagtouse = highestprogrssive() 
