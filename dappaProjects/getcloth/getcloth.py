cfcce2cb328075044e6af239a6c5e9bb8107c97d
MaxMegaman91 <aarush.ar@gmail.com>
2022-10-19 18:52:13 -0400

    dftd oct19 -linux

--- dappaProjects/getcloth/getcloth.py
+++ dappaProjects/getcloth/getcloth.py
@@ -1,3 +1,4 @@
+SAVE_PATH = "/home/aarush/ChummaCode/dappaProjects/getcloth/"
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os, sys, json
@@ -7,7 +8,9 @@ def getlink(url):
     return soup
 
 webhtml = getlink("https://weather.com/en-CA/weather/hourbyhour/l/584018bec07ce9573837c14fa59da031fa6fcdeb1c3c9e3b2b27cb79ce254b5a")
-for t in webhtml.find_all("span", ):
-    print(t.text)
+for t in webhtml.find_all("script"):
+    if str(t).find("window.__data=JSON.parse") != -1:
+        with open(SAVE_PATH+"getcloth.txt", "w") as f:
+            f.write(str(t.text).replace("\\\"", "\""))
 
 #   view-source:https://weather.com/en-CA/weather/hourbyhour/l/584018bec07ce9573837c14fa59da031fa6fcdeb1c3c9e3b2b27cb79ce254b5a
