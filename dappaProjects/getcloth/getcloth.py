59e3f772ee1b06b36e10a74dff8227555f239688
MaxMegaman91 <aarush.ar@gmail.com>
2022-10-19 17:43:40 -0400

    push -w11

--- dappaProjects/getcloth/getcloth.py
+++ dappaProjects/getcloth/getcloth.py
@@ -4,4 +4,10 @@ import os, sys, json
 
 def getlink(url):
     soup = BeautifulSoup(urlopen(url).read().decode("utf-8"),"html.parser")
-    jsontext = json.loads(soup.text)
+    return soup
+
+webhtml = getlink("https://weather.com/en-CA/weather/hourbyhour/l/584018bec07ce9573837c14fa59da031fa6fcdeb1c3c9e3b2b27cb79ce254b5a")
+for t in webhtml.find_all("span", ):
+    print(t.text)
+
+#   view-source:https://weather.com/en-CA/weather/hourbyhour/l/584018bec07ce9573837c14fa59da031fa6fcdeb1c3c9e3b2b27cb79ce254b5a
